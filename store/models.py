ce52a0e72820a39781dc1738247555be0a38c0d4
Elijah Ofori <kobbyofori9@gmail.com>
2024-03-02 22:50:46 +0000

    Initial commit for Mama Linda's Special Shito project, set up Ecommerce python project with Django, initialized project structure and configuration files, and created models.

---
+++ store/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+import datetime
+
+
+# Products Category Model
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# Products Model
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="uploads/productsImg")
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+# Customer Model
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# Customer Order Model
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    phone = models.CharField(max_length=20, default="")
+    address = models.CharField(max_length=200)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
