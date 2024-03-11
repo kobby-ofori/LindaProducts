0ae9ec6b36ef45b35a658d259e104fbd9b19bc34
Elijah Ofori <kobbyofori9@gmail.com>
2024-03-11 22:07:04 +0000

    Created sign-in and sign-up pages

--- store/views.py
+++ store/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {})
+
+
+def signin(request):
+    return render(request, "signin.html", {})
+
+
+def signup(request):
+    return render(request, "signup.html", {})
