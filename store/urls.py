6e327900ddc0eee19b1544fef285d29534259940
Elijah Ofori <kobbyofori9@gmail.com>
2024-03-12 23:38:43 +0000

    Display of products on home page done! created product page, login and logout authentication completed.

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("signin/", views.signin, name="signin/"),
-    path("signup/", views.signup, name="signup/")
+    path("signin/", views.signin, name="signin"),
+    path("signup/", views.signup, name="signup"),
+    path("logout/", views.logout_user, name="logout"),
+    path("product/<int:pk>", views.product_page, name="product"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, "home.html", {})
+    products = Product.objects.all()
+    return render(request, "home.html", {'products': products})
+
+
+def product_page(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, "product_page.html", {'product': product})
 
 
 def signin(request):
-    return render(request, "signin.html", {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "log-in successful")
+            return redirect('home')
+        else:
+            messages.success(request, "Credentials error, please try again")
+            return redirect('signin')
+    else:
+        return render(request, "signin.html", {})
 
 
 def signup(request):
     return render(request, "signup.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "log-out successful")
+    return redirect('home')
